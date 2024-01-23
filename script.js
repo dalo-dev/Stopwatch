f81de5363765396072354c95914eef7b391f0c4c
DALO <dalodev@proton.me>
2024-01-23 17:54:08 -0500

    Add start feature

--- script.js
+++ script.js
@@ -4,9 +4,32 @@ let minutes = 0;
 let hours = 0;
 
 const timeElement = document.getElementById("time");
+const startElement = document.getElementById("start-btn");
 
 timeElement.innerText = `${String(hours).padStart(2, "0")}:${String(
   minutes
 ).padStart(2, "0")}:${String(seconds).padStart(2, "0")}:${String(
   milliseconds
 ).padStart(2, "0")}`;
+
+const addSecond = function () {
+  seconds++;
+  if (seconds > 59) {
+    seconds = 0;
+    minutes++;
+  }
+  if (minutes > 59) {
+    minutes = 0;
+    hours++;
+  }
+
+  timeElement.innerText = `${String(hours).padStart(2, "0")}:${String(
+    minutes
+  ).padStart(2, "0")}:${String(seconds).padStart(2, "0")}:${String(
+    milliseconds
+  ).padStart(2, "0")}`;
+
+  setTimeout(addSecond, 1000);
+};
+
+startElement.addEventListener("click", addSecond);
